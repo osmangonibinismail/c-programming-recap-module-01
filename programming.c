e77c5ed83bc86464fb763e875d81aa4df1354750
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-28 02:20:21 +0600

    hacker rank problem solve

--- programming.c
+++ programming.c
@@ -367,47 +367,66 @@
 //     return 0;
 // }
 
-#include<stdio.h>
-int main(){
+// #include<stdio.h>
+// int main(){
 
+//     int n;
+//     scanf("%d", &n);
+
+//     int a[n + 5];
+
+//     for(int i = 0; i < n; i++){
+//         scanf("%d", &a[i]);
+//     }
+
+//     // 1. count the min value
+
+//     int min = a[0];
+
+//     for(int i = 0; i < n; i++){
+//         if(a[i] < min){
+//             min = a[i];
+//         }
+//     }
+
+//     // printf("min = %d\n", min);
+
+//     // 2. count the frequency of min
+
+//     int cnt = 0;
+
+//     for(int i = 0; i < n; i++){
+//         if(a[i] == min){
+//             cnt++;
+//         }
+//     }
+
+//     // printf("frequency = %d\n", cnt);
+
+//     // // 3. is the frequency odd/even
+
+//     if(cnt % 2 == 0){
+//         printf("Unlucky\n");
+//     }else{
+//         printf("Lucky\n");
+//     }
+// }
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     int n;
     scanf("%d", &n);
-
-    int a[n + 5];
-
+    
     for(int i = 0; i < n; i++){
-        scanf("%d", &a[i]);
+       printf("I Love Practice\n"); 
     }
-
-    // 1. count the min value
-
-    int min = a[0];
-
-    for(int i = 0; i < n; i++){
-        if(a[i] < min){
-            min = a[i];
-        }
-    }
-
-    // printf("min = %d\n", min);
-
-    // 2. count the frequency of min
-
-    int cnt = 0;
-
-    for(int i = 0; i < n; i++){
-        if(a[i] == min){
-            cnt++;
-        }
-    }
-
-    // printf("frequency = %d\n", cnt);
-
-    // // 3. is the frequency odd/even
-
-    if(cnt % 2 == 0){
-        printf("Unlucky\n");
-    }else{
-        printf("Lucky\n");
-    }
+    
+    
+    return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,44 @@
-n);
+#include<stdio.h>
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    int a[n + 5];
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    // 1. count the min value
+
+    int min = a[0];
+
+    for(int i = 0; i < n; i++){
+        if(a[i] < min){
+            min = a[i];
+        }
+    }
+
+    // printf("min = %d\n", min);
+
+    // 2. count the frequency of min
+
+    int cnt = 0;
+
+    for(int i = 0; i < n; i++){
+        if(a[i] == min){
+            cnt++;
+        }
+    }
+
+    // printf("frequency = %d\n", cnt);
+
+    // // 3. is the frequency odd/even
+
+    if(cnt % 2 == 0){
+        printf("Unlucky\n");
+    }else{
+        printf("Lucky\n");
+    }
+}
